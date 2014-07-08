beb0406a4b9665bdd66d6b6ce0a0f1ed52bfeb0f
杉田　寿憲 <s.toshi0607@gmail.com>
2014-07-09 01:42:24 +0900

    検索条件追加　モデル ・用途 ・職業

--- app/Controller/SukesansController.php
+++ app/Controller/SukesansController.php
@@ -19,9 +19,25 @@ class SukesansController extends AppController{
         $this->Session->write('session',$this->request->data);
         $condition = $this->Session->read('session');
         $this->Condition->save($condition);//検索条件を検索条件DBに保存
+        
+
         $location = $condition['Condition']['location'];
+        $occupation = $condition['Condition']['occupation'];
+        $purpose = $condition['Condition']['purpose'];
+
+        
         $params = $this->Shop->find('all', array(//検索条件から店舗情報DBを検索
-            'conditions' => array('Shop.location LIKE' => '%'.$location.'%'),
+            'conditions' => array(
+                
+                //最寄り駅検索   
+                'Shop.location LIKE' => '%'.$location.'%',
+                //利用シーン検索
+                'Shop.purpose' => $purpose,
+                //絞り込み検索
+                'Shop.occupation' => $occupation
+
+
+            ),
             'order' => array('Shop.review DESC')//食べログ評価の大きい順に並び替え
             )
         );
